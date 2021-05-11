fcbed32e2a1bc9e9ae6db95dc64d3e265aff735d
jeysonlr <jeysonlr@gmail.com>
2021-05-11 20:48:36 -0300

    firts commit

---
+++ README.md
@@ -0,0 +1,27 @@
+
+
+##  Script utilizado para baixar audios no formato mp3 direto de um video do YouTube.
+
+
+## Pré-requisitos :pushpin:
+
+```
+pip install pytube
+```
+
+```
+pip install moviepy
+```
+
+## Como rodar a aplicação :arrow_forward:
+
+No terminal navegar até o diretório onde se encontram os arquivos, e digitar:
+
+```
+python downloadMP3.py
+```
+
+## Bibliotecas utilizadas :books:
+
+- [MoviePy](https://pypi.org/project/moviepy/)
+- [PyTube](https://pypi.org/project/pytube/)
---
+++ downloadMP3.py
@@ -0,0 +1,26 @@
+from pytube import YouTube
+import moviepy.editor as mp
+import re
+import os
+
+# endereco do video e endereco onde salvar
+link = input("Digite o link que deseja baixar: ")
+path = input("Digite o caminho onde deseja salvar o arquivo: ")
+yt = YouTube(link)
+
+# comeca o dowload
+print("Baixando...")
+ys = yt.streams.filter(only_audio=True).first().download(path)
+print("Download completo!!!")
+
+#converte para mp3
+print("Convertendo arquivo...")
+for file in os.listdir(path):
+    if re.search('mp4', file):
+        mp4_path = os.path.join(path, file)
+        mp3_path = os.path.join(path, os.path.splitext(file)[0]+'.mp3')
+        new_file = mp.AudioFileClip(mp4_path)
+        new_file.write_audiofile(mp3_path)
+        os.remove(mp4_path)
+
+print("Sucesso")
